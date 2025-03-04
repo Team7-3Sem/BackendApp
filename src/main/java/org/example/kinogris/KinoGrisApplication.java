32381e588400d23f76398de94edd5cfc938eead2
Kalle-KO <kaot0001@stud.kea.dk>
2025-03-04 12:51:10 +0100

    struktur

---
+++ src/main/java/org/example/kinogris/KinoGrisApplication.java
@@ -0,0 +1,13 @@
+package org.example.kinogris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoGrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoGrisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kinogris/KinoGrisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kinogris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoGrisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
