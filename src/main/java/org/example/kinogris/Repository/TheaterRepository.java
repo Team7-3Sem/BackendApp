a3f09926c7e07af350b4fd66e8481f807914774a
JullerMus <juliusclykkegaard@gmail.com>
2025-03-05 12:04:55 +0100

    2.1.2 Implement theater configuration API endpoints

---
+++ src/main/java/org/example/kinogris/Repository/TheaterRepository.java
@@ -0,0 +1,7 @@
+package org.example.kinogris.Repository;
+
+import org.example.kinogris.Model.Theater;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheaterRepository extends JpaRepository<Theater, Integer> {
+}
--- src/main/java/org/example/kinogris/Service/TheaterService.java
+++ src/main/java/org/example/kinogris/Service/TheaterService.java
@@ -1,7 +1,49 @@
 package org.example.kinogris.Service;
 
+import org.example.kinogris.Model.Theater;
+import org.example.kinogris.Repository.TheaterRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class TheaterService {
+
+    private final TheaterRepository theaterRepository;
+
+    public TheaterService(TheaterRepository theaterRepository) {
+        this.theaterRepository = theaterRepository;
+    }
+
+    public ResponseEntity<Theater> createTheater(Theater theater) {
+        theaterRepository.save(theater);
+        return new ResponseEntity<>(theater, HttpStatus.CREATED);
+    }
+
+    public Set<Theater> getTheatres(){
+        Set<Theater> theaters = new HashSet<>();
+        theaters.addAll(theaterRepository.findAll());
+        return theaters;
+    }
+
+    public ResponseEntity<Theater> getTheaterById(int theaterId){
+        Optional<Theater> theater = theaterRepository.findById(theaterId);
+        return theater.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    /**
+     *
+     * @param theaterId
+     * @return
+     */
+    public List<Integer> getTheaterLayoutById(int theaterId){
+        List<Integer> theaterLayout = new ArrayList<>();
+        Theater theater = theaterRepository.findById(theaterId).get();
+        //Optional<Theater> theater = theaterRepository.findById(theaterId);
+        theaterLayout.add(theater.getRowCount());
+        theaterLayout.add(theater.getSeatsPerRow());
+        return theaterLayout;
+    }
 }
