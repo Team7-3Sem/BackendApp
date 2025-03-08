290ab21b2383f6a0427aeb62e84e2191ac16097b
JullerMus <juliusclykkegaard@gmail.com>
2025-03-08 11:01:01 +0100

    Testing CRUD for all entities using HTTP client

--- src/main/java/org/example/kinogris/Controller/MovieController.java
+++ src/main/java/org/example/kinogris/Controller/MovieController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("")
 public class MovieController {
 
     private final MovieService movieService;
