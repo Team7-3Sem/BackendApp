a55e6c5c149276ff1876322c827312d14c2a68f8
KasperM2000 <kasper.madsen@busted.dk>
2025-03-10 10:37:17 +0100

    Added ReservationController and ReservationService

--- src/main/java/org/example/kinogris/Repository/ReservationRepository.java
+++ src/main/java/org/example/kinogris/Repository/ReservationRepository.java
@@ -4,9 +4,10 @@ import org.example.kinogris.Model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
-    Set<Reservation> findById(int id);
+    Optional<Reservation> findById(int id);
 }
